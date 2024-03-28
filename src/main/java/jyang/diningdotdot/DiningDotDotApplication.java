99b79f853b72163bab4fef016f3a58abc3ae00de
JaeJun <yyggb12@kakao.com>
2024-03-28 18:55:59 +0900

    init

---
+++ src/main/java/jyang/diningdotdot/DiningDotDotApplication.java
@@ -0,0 +1,13 @@
+package jyang.diningdotdot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningDotDotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningDotDotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jyang/diningdotdot/DiningDotDotApplicationTests.java
@@ -0,0 +1,13 @@
+package jyang.diningdotdot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningDotDotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
