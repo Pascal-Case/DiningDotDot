b785e47e59baf40f2feaf54484257f2782261614
JaeJun <yyggb12@kakao.com>
2024-04-06 20:30:24 +0900

    refactoring: 정적파일 spring security 무시 기능 분리

--- src/main/java/jyang/diningdotdot/config/SecurityConfig.java
+++ src/main/java/jyang/diningdotdot/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,7 +31,6 @@ public class SecurityConfig {
                         .requestMatchers("/", "/login", "/main/**", "/stores/**",
                                 "/users/join", "/users/joinProc",
                                 "/partners/join", "/partners/joinProc").permitAll()
-                        .requestMatchers("/js/**", "/css/**", "/img/**").permitAll() // 정적 파일
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated() // 그외 경로는 로그인 사용자만 허용
                 );
@@ -66,5 +66,10 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/"));
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/js/**", "/css/**", "/img/**");
     }
 }
